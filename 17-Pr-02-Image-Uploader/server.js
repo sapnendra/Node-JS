8ad68cf830ecf7c1612d04893a1cf6805c11bcdf
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-11-23 13:17:10 +0530

    add image uploader pro

--- 17-Pr-02-Image-Uploader/server.js
+++ 17-Pr-02-Image-Uploader/server.js
@@ -2,8 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
+const PORT = 3000;
 
-const PORT = 1000;
+mongoose
+  .connect("mongodb://localhost:27017/LearningNodeJS")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Error:", err));
+
+// rendering ejs file
+app.get("/", (req, res) => {
+  res.render("index.ejs", { url: null });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
